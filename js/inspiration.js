8ff4c663dd3683f80415f049d7d334fa2cce6b79
Benjamin Friedman <benjamin.friedman97@gmail.com>
2023-06-27 02:31:29 -0400

    add songs

--- js/inspiration.js
+++ js/inspiration.js
@@ -1,7 +1,7 @@
 const musicians = {
     'hans-zimmer': {
         songs: ['Why Do We Fall', 'Agent Of Chaos', 'Molossus', "Gotham's Reckoning", 'Imagine The Fire', 'Necessary Evil', 'Nothing Out There', 'Rise', 'A Dark Knight',
-                'Time', 'Projections', 'First Step', 'No Time For Caution', 'Mountains', 'Final Ascent', 'Goodbye My Son', 'Is She With You', 'Marry Me Suite', 'Davy Jones',
+                'Final Ascent', 'Time', 'Projections', 'First Step', 'No Time For Caution', 'Mountains', 'Goodbye My Son', 'Is She With You', 'Marry Me Suite', 'Davy Jones',
                 'Not In Blood But In Bond', 'Ah, Putrefaction', 'Now We Are Free', 'Now We Are Free - Cello'],
         idx: 0
     },
