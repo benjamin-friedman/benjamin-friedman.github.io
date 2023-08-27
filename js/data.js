6e54cd236f2a0127d72d5a565bc061d031ada951
Benjamin Friedman <benjamin.friedman97@gmail.com>
2023-08-26 22:51:25 -0400

    edit songs

--- js/data.js
+++ js/data.js
@@ -89,7 +89,8 @@ export const musiciansArray = [
         name: 'Schubert',
         songs: [
             { name: 'Impromptu In G Flat Major Op. 90 No. 3, D. 899', recordingMusician: 'Olga Jegunova' },
-            { name: 'Piano Trio No. 2 In E Flat Major 2nd Movement Op. 100, D. 929', recordingMusician: 'Unknown' },
+            { name: 'Piano Trio No. 2 In E Flat Major 2nd Movement Op. 100, D. 929 - Interpretation 1', recordingMusician: 'Unknown' },
+            { name: 'Piano Trio No. 2 In E Flat Major 2nd Movement Op. 100, D. 929 - Interpretation 2', recordingMusician: 'Unknown' },
             { name: 'Serenade D. 889 - Interpretation 1', recordingMusician: 'Unknown' },
             { name: 'Serenade D. 889 - Interpretation 2', recordingMusician: 'Unknown' },
             { name: 'Serenade D. 889 - Piano and Cello', recordingMusician: 'Stjepan Hauser' },
